f0b8f9eb87e5b11e1706b81a8ddc961b7dcd7150
Ben Askins <ben.askins@gmail.com>
2009-03-30 05:15:58 +0800

    Drying up
    Signed-off-by: Lachlan Hardy <lachlan@lachstock.com.au>

--- streamslide.rb
+++ streamslide.rb
@@ -47,23 +47,16 @@ helpers do
   def flickr_search(tags)
     plain_tags, user_tags = tag_sorting(tags)
 
-    plain_query = "SELECT * FROM flickr.photos.sizes WHERE label=\"Large\" AND photo_id IN (SELECT id FROM flickr.photos.search WHERE #{plain_tags.map {|t| "tags = '#{t}'"}.join(" OR ")})"
-    url = "http://query.yahooapis.com/v1/public/yql?q=#{URI.encode(plain_query)}&format=json"
-    result = JSON.parse(Net::HTTP.get_response(URI.parse(url)).body)
+    result = yql_query("SELECT * FROM flickr.photos.sizes WHERE label=\"Large\" AND photo_id IN (SELECT id FROM flickr.photos.search WHERE #{plain_tags.map {|t| "tags = '#{t}'"}.join(" OR ")})")
     
-    
-    user_query = "select href from html where url=\"http://flickr.com/photos/lachlanhardy/tags/whippet\" and xpath=\'//span[@class=\"photo_container pc_t\"]/a[@href]\'"
-    user_url = "http://query.yahooapis.com/v1/public/yql?q=#{URI.encode(user_query)}&format=json"
-    user_query_result = JSON.parse(Net::HTTP.get_response(URI.parse(user_url)).body)
+    user_query_result = yql_query("select href from html where url=\"http://flickr.com/photos/lachlanhardy/tags/whippet\" and xpath=\'//span[@class=\"photo_container pc_t\"]/a[@href]\'")
     
     user_tag_array = []
     user_query_result['query']['results']['a'].each do |id|
       user_tag_array.push id['href'].gsub(/\/photos\/.+\/(.+)\//, '\1')
     end
-    
-    user_query2 = "SELECT * FROM flickr.photos.sizes WHERE label=\"Large\" AND #{user_tag_array.map {|t| "photo_id = '#{t}'"}.join(" OR ")}"
-    user_url = "http://query.yahooapis.com/v1/public/yql?q=#{URI.encode(user_query2)}&format=json"
-    user_query_result2 = JSON.parse(Net::HTTP.get_response(URI.parse(user_url)).body)
+
+    user_query_result2 = yql_query("SELECT * FROM flickr.photos.sizes WHERE label=\"Large\" AND #{user_tag_array.map {|t| "photo_id = '#{t}'"}.join(" OR ")}")
     
     puts user_query_result2
     
@@ -91,5 +84,9 @@ helpers do
     return plain_tags, user_tags
   end
   
+  def yql_query(query_string)
+    url = "http://query.yahooapis.com/v1/public/yql?q=#{URI.encode(query_string)}&format=json"
+    JSON.parse(Net::HTTP.get_response(URI.parse(url)).body)
+  end
 end
 
