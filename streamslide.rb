85d42a859e53dbf420a8ed7ef34f03d2be23f938
Lachlan Hardy <lachlan@lachstock.com.au>
2009-03-30 00:42:31 +1100

    gross, but partway there

--- streamslide.rb
+++ streamslide.rb
@@ -21,6 +21,7 @@ end
 get '/:tags' do 
   @tags = params[:tags]
   @photos = flickr_search(@tags.split(","))
+  puts @photos
   view :index
 end
 
@@ -44,13 +45,51 @@ helpers do
   end
   
   def flickr_search(tags)
-    query = "SELECT * FROM flickr.photos.sizes WHERE label=\"Large\" AND photo_id IN (SELECT id FROM flickr.photos.search WHERE #{tags.map {|t| "tags = '#{t}'"}.join(" OR ")})"
-    url = "http://query.yahooapis.com/v1/public/yql?q=#{URI.encode(query)}&format=json"
-    result = JSON.parse(Net::HTTP.get_response(URI.parse(url)).body)
+    plain_tags, user_tags = tag_sorting(tags)
 
+    plain_query = "SELECT * FROM flickr.photos.sizes WHERE label=\"Large\" AND photo_id IN (SELECT id FROM flickr.photos.search WHERE #{plain_tags.map {|t| "tags = '#{t}'"}.join(" OR ")})"
+    url = "http://query.yahooapis.com/v1/public/yql?q=#{URI.encode(plain_query)}&format=json"
+    result = JSON.parse(Net::HTTP.get_response(URI.parse(url)).body)
+    
+    
+    user_query = "select href from html where url=\"http://flickr.com/photos/lachlanhardy/tags/whippet\" and xpath=\'//span[@class=\"photo_container pc_t\"]/a[@href]\'"
+    user_url = "http://query.yahooapis.com/v1/public/yql?q=#{URI.encode(user_query)}&format=json"
+    user_query_result = JSON.parse(Net::HTTP.get_response(URI.parse(user_url)).body)
+    
+    user_tag_array = []
+    user_query_result['query']['results']['a'].each do |id|
+      user_tag_array.push id['href'].gsub(/\/photos\/.+\/(.+)\//, '\1')
+    end
+    
+    user_query2 = "SELECT * FROM flickr.photos.sizes WHERE label=\"Large\" AND #{user_tag_array.map {|t| "photo_id = '#{t}'"}.join(" OR ")}"
+    user_url = "http://query.yahooapis.com/v1/public/yql?q=#{URI.encode(user_query2)}&format=json"
+    user_query_result2 = JSON.parse(Net::HTTP.get_response(URI.parse(user_url)).body)
+    
+    puts user_query_result2
+    
+    result = result.merge(user_query_result2)
+    
+    puts result
+    
+    
     raise "web service error" if result.has_key? 'Error'
-     
     return result['query']['results']['size']
   end
+  
+  def tag_sorting(tags)
+    plain_tags = []
+    user_tags = []
+    
+    tags.each do |tag|
+      if tag[/:/]
+        user_tags.push tag
+      else
+        plain_tags.push tag
+      end
+    end
+    
+    return plain_tags, user_tags
+  end
+  
 end
 
